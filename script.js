808256f5e474fb51e89ba003375c14acce23eead
aqtagon <ahmed.danawi@gmail.com>
2023-10-31 18:18:32 -0700

    Added element id functions to js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('search-form');
+    const cityInput = document.getElementById('city-input');
+    const weatherInfo = document.getElementById('weather-info');
+    const forecastInfo = document.getElementById('forecast-info');
+
+    
 })
